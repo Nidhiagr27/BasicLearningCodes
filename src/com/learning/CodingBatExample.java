f92e3659f3afef5a197f3abb91de015a3435f413
Nidhi Agrawal <nidhiagr1327@gmail.com>
2022-06-08 13:53:57 +0530

    string+hashMap

---
+++ src/com/learning/CodingBatExample.java
@@ -0,0 +1,33 @@
+package com.learning;// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class CodingBatExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        String[] op = abc(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"});
+        for(int i=0; i<op.length; i++) {
+            System.out.print(", ");
+            System.out.print(op[i]);
+        }
+        System.out.println();
+    }
+    public static String[] abc(String[] strings) {
+        Map<Character, Integer> lu = new HashMap<>();
+        
+        for(int i=0; i<strings.length; i++) {
+
+            Character currentChar = strings[i].charAt(0);
+            if(lu.containsKey(currentChar)) {
+                String temp = strings[i];
+                strings[i] = strings[lu.get(currentChar)];
+                strings[lu.get(currentChar)] =  temp;
+                lu.remove(currentChar);
+            } else {
+                lu.put(currentChar, i);
+            }
+        }
+        
+        return strings;
+    }
+}
