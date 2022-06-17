0d8fdb419179d9ff6f66658345d3f4d8e3d8d54b
Nidhi Agrawal <nidhiagr1327@gmail.com>
2022-06-17 13:58:53 +0530

    insert at front and after particular index.

--- src/linkedListImplementation/LinkedList.java
+++ src/linkedListImplementation/LinkedList.java
@@ -28,4 +28,32 @@ public class LinkedList {
             curr.setNext(newNode);
         }
     }
+    public void insertAtFront(char data){
+        Node newNode=new Node(data,null);
+        if(head==null){
+            head=newNode;
+        }
+        else{
+            newNode.setNext(head);
+            head=newNode;
+        }
+    }
+
+    public void insertAfter(int position,char data){
+        Node newNode=new Node(data,null);
+        int currentIndex=0;
+        Node curr=head;
+        while(currentIndex<position && curr.getNext()!=null){
+            curr=curr.getNext();
+            currentIndex++;
+        }
+        if(curr!=null) {
+            newNode.setNext(curr.getNext());
+            curr.setNext(newNode);
+        }
+        else{
+            head=newNode;
+        }
+    }
+
 }
--- src/linkedListImplementation/Main.java
+++ src/linkedListImplementation/Main.java
@@ -3,11 +3,20 @@ package linkedListImplementation;
 public class Main {
     public static void main(String[] args){
         LinkedList ll=new LinkedList();
+
+        //testing insert at end
         ll.insertAtEnd('E');
         ll.insertAtEnd('D');
         ll.insertAtEnd('C');
         ll.insertAtEnd('B');
         ll.insertAtEnd('A');
+
+        //testing insert at front
+        ll.insertAtFront('a');
+
+        //testing insert after particular index
+        ll.insertAfter(1,'5');
+        ll.insertAfter(0,'8');
 
         ll.traverse();
     }
